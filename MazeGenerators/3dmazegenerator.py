37d03f61fdfdec3341741ca576ab6caa9c23994c
NathansProgramming <108202470+NathansProgramming@users.noreply.github.com>
2022-09-11 10:44:04 -0600

    Added all the files
    . Added file that contains all of the maze generators
    . Added file that contains all of the maze examples
    . Added a README.md file that explains how the code works and what things to know when using it.

---
+++ MazeGenerators/3dmazegenerator.py
@@ -0,0 +1,261 @@
+import random
+# if your computer or device can't run colorama, just delete these two lines and remove "FORE.<color>" in print_maze.
+from colorama import init, Fore
+init()
+
+# this text controls what you see. Default is 'C', '#', 'U', 'D'.
+cell = 'C'
+wall = '#'
+up = 'U'
+down = 'D'
+
+# these control how big your maze is. Change them to what you want.
+# 4x4x1 is the smallest you can go with it.
+length = 10
+width = 10
+depth = 10
+
+
+# 3d maze coordinate logic works rather similarly to 2d maze coordinate logic, except with the added z.
+# due to depth being added, the new coordinate system is (z, y, x) and depth starts at the top and goes to the bottom.
+def make_3d_maze(ln, wd, dp):
+    """Creates maze by making a triple nested list with all the characters being walls."""
+    maze = []  # the list that will hold the maze.
+    for z in range(0, dp):  # keep making boards of lines for how much the depth is.
+        board = []  # a board stores a series of lines.
+        for y in range(0, wd):  # keep making lines of characters for how much width there is.
+            line = []  # a line stores characters.
+            for x in range(0, ln):
+                line.append(wall)  # appends a wall to the list
+            board.append(line)  # appends the list to the board once all of the walls have been appended.
+        maze.append(board)  # appends the board to the maze once all of the lines have been appended.
+    return maze  # once finished, return the maze.
+
+
+def print_maze(maze, ln, wd, dp):
+    """Prints the maze with colors (or not) in an organized and easy to read way."""
+    for z in range(0, dp):  # for as many boards as there are in the maze
+        for y in range(0, wd):  # for as many lines as there are in the board
+            for x in range(0, ln):  # for as many characters as there are in the line
+                if maze[z][y][x] == cell:  # if the character is a cell, make it green.
+                    print(Fore.GREEN, f'{maze[z][y][x]}', end="")
+                elif maze[z][y][x] == wall:  # if the character is a wall, make it red.
+                    print(Fore.RED, f'{maze[z][y][x]}', end="")
+                elif maze[z][y][x] == down:  # if the character is an up, make it blue.
+                    print(Fore.BLUE, f'{maze[z][y][x]}', end="")
+                else:  # if the character is a down or an invalid character is shown, make it yellow.
+                    print(Fore.YELLOW, f'{maze[z][y][x]}', end="")
+            print('')
+        print('\n')
+
+
+the_maze = make_3d_maze(length, width, depth)  # create the maze based on the length, width and depth specified.
+# I'm sorry if it hurts you that all the following code below is not in a def function. I made it to be simple.
+
+starting_z = random.randint(0, depth-1)  # makes a random z-coordinate
+starting_y = random.randint(0, width-1)  # makes a random y-coordinate
+starting_x = random.randint(0, length-1)  # makes a random x-coordinate
+
+# here, we make sure that the starting coordinate cell isn't going to clip into a wall.
+# if you're wondering why it's -2 on some of them instead of just -1, it's just to be safe on the bug side.
+# honestly, the -2's could be removed. Could be a potential test just to see how it functions.
+if starting_x == 0:  # if starting_x is on the edge, change the position.
+    starting_x += 2
+if starting_x == 1:
+    starting_x += 1
+if starting_x == length - 1:  # vice versa.
+    starting_x -= 2
+if starting_x == length - 2:
+    starting_x -= 1
+if starting_y == 0:  # if starting_y is on the edge, change the position.
+    starting_y += 2
+if starting_y == 1:
+    starting_y += 1
+if starting_y == width - 1:  # vice versa.
+    starting_y -= 2
+if starting_y == width - 2:
+    starting_y -= 1
+# depth doesn't need this check because unlike the last two, it does not require specific positioning.
+
+# make the random start position in the maze a cell.
+the_maze[starting_z][starting_y][starting_x] = cell
+
+# add in the walls that are around the beginning cell to begin the fun.
+walls = [[starting_z, starting_y - 1, starting_x], [starting_z, starting_y, starting_x - 1],
+         [starting_z, starting_y, starting_x + 1], [starting_z, starting_y + 1, starting_x],
+         [starting_z - 1, starting_y, starting_x], [starting_z + 1, starting_y, starting_z]]
+
+while len(walls) > 0:  # while there are still walls in the walls list. Yes, it does have to be len(walls).
+    rando_choice = random.randint(0, len(walls) - 1)  # gets a random index to choose from
+    random_wall = walls[rando_choice]  # uses that random index to get a random wall from the walls list.
+    if random_wall[0] < 0 or random_wall[0] > depth - 1:  # if the wall is out of bounds, remove and start again.
+        del walls[rando_choice]
+        continue
+    if random_wall[1] < 1 or random_wall[1] > width - 2:
+        del walls[rando_choice]
+        continue
+    if random_wall[2] < 1 or random_wall[2] > length - 2:
+        del walls[rando_choice]
+        continue
+    if the_maze[random_wall[0]][random_wall[1]][random_wall[2]] != wall:  # if the "wall" isn't a wall, remove.
+        del walls[rando_choice]
+        continue
+
+    # keeps track of how many cells are nearby the wall. In dimensions of 3 and higher, it has to be ONLY 1.
+    # if the cell count is 1, that wall turns into a cell or other equivalent. If lower or higher, forget and remove.
+    cell_count = 0
+    if random_wall[0] - 1 < 0:  # if the random wall's neighbor is out of bounds, don't check if it's a cell.
+        cell_count = cell_count  # random code to do something other than nothing or something bad
+    elif the_maze[random_wall[0] - 1][random_wall[1]][random_wall[2]] != wall:  # if the neighbor isn't a wall, increment cell count.
+        cell_count += 1
+
+    if random_wall[0] + 1 > depth - 1:
+        cell_count = cell_count
+    elif the_maze[random_wall[0] + 1][random_wall[1]][random_wall[2]] != wall:
+        cell_count += 1
+
+    if random_wall[1] - 1 < 0:
+        cell_count = cell_count
+    elif the_maze[random_wall[0]][random_wall[1] - 1][random_wall[2]] != wall:
+        cell_count += 1
+
+    if random_wall[1] + 1 > width - 1:
+        cell_count = cell_count
+    elif the_maze[random_wall[0]][random_wall[1] + 1][random_wall[2]] != wall:
+        cell_count += 1
+
+    if random_wall[2] - 1 < 0:
+        cell_count = cell_count
+    elif the_maze[random_wall[0]][random_wall[1]][random_wall[2] - 1] != wall:
+        cell_count += 1
+
+    if random_wall[2] + 1 > length - 1:
+        cell_count = cell_count
+    elif the_maze[random_wall[0]][random_wall[1]][random_wall[2] + 1] != wall:
+        cell_count += 1
+
+    if cell_count == 1:  # if the wall passed the skill check, determine where the one cell it came from was at.
+        while True:  # this loop is used primarily to catch bugs, but is also just a nice safety net in general.
+            if random_wall[0] - 1 < 0:
+                cell_count = cell_count
+                # note that "is a cell" down below really means "isn't a wall"
+            elif the_maze[random_wall[0] - 1][random_wall[1]][random_wall[2]] != wall:  # if the block above is a cell,
+                the_maze[random_wall[0] - 1][random_wall[1]][random_wall[2]] = down  # make it a down
+                the_maze[random_wall[0]][random_wall[1]][random_wall[2]] = up  # make the current block an up.
+                break
+
+            if random_wall[0] + 1 > depth - 1:
+                cell_count = cell_count
+            elif the_maze[random_wall[0] + 1][random_wall[1]][random_wall[2]] != wall:  # if the block below is a cell,
+                the_maze[random_wall[0] + 1][random_wall[1]][random_wall[2]] = up  # make it an up
+                the_maze[random_wall[0]][random_wall[1]][random_wall[2]] = down  # make the current block a down.
+                break
+
+            if random_wall[1] - 1 < 0:
+                cell_count = cell_count
+            elif the_maze[random_wall[0]][random_wall[1] - 1][random_wall[2]] != wall:  # if the block north is a cell,
+                the_maze[random_wall[0]][random_wall[1]][random_wall[2]] = cell  # make the current block a cell.
+                break
+
+            if random_wall[1] + 1 > width - 1:
+                cell_count = cell_count
+            elif the_maze[random_wall[0]][random_wall[1] + 1][random_wall[2]] != wall:  # if the block south is a cell,
+                the_maze[random_wall[0]][random_wall[1]][random_wall[2]] = cell  # make the current block a cell.
+                break
+
+            if random_wall[2] - 1 < 0:
+                cell_count = cell_count
+            elif the_maze[random_wall[0]][random_wall[1]][random_wall[2] - 1] != wall:  # if the block west is a cell,
+                the_maze[random_wall[0]][random_wall[1]][random_wall[2]] = cell  # make the current block a cell.
+                break
+
+            if random_wall[2] + 1 > length - 1:
+                cell_count = cell_count
+            elif the_maze[random_wall[0]][random_wall[1]][random_wall[2] + 1] != wall:  # if the block east is a cell,
+                the_maze[random_wall[0]][random_wall[1]][random_wall[2]] = cell  # make the current block a cell.
+                break
+
+        # attempt to add all neighboring walls, including ones above and below the cell.
+        try:
+            walls.append([random_wall[0] - 1, random_wall[1], random_wall[2]])
+        except IndexError:
+            pass
+        try:
+            walls.append([random_wall[0] + 1, random_wall[1], random_wall[2]])
+        except IndexError:
+            pass
+        try:
+            walls.append([random_wall[0], random_wall[1] - 1, random_wall[2]])
+        except IndexError:
+            pass
+        try:
+            walls.append([random_wall[0], random_wall[1] + 1, random_wall[2]])
+        except IndexError:
+            pass
+        try:
+            walls.append([random_wall[0], random_wall[1], random_wall[2] - 1])
+        except IndexError:
+            pass
+        try:
+            walls.append([random_wall[0], random_wall[1], random_wall[2] + 1])
+        except IndexError:
+            pass
+
+        del walls[rando_choice]  # once done with adding neighboring walls, delete itself in the walls list
+    else:  # if the wall had 2 or more cells nearby, delete itself in the walls list and go back to the start
+        del walls[rando_choice]
+
+# don't change any of these values. they function on their own.
+# these values make sure that an entrance and an exit to the maze are made.
+# explanation for how this works can be found in the 2d maze generator. This is similar, but goes in two directions.
+a = 0
+b = 1
+c = 1
+the_maze[a][b - 1][c] = cell
+constant = 1
+while True:
+    if constant == 1:
+        if the_maze[a][b][c] == wall:
+            the_maze[a][b][c] = cell
+        else:
+            break
+    c += constant
+    if the_maze[a][b][c] == wall:
+        the_maze[a][b][c] = cell
+    else:
+        break
+    c -= constant
+    b += constant
+    if the_maze[a][b][c] == wall:
+        the_maze[a][b][c] = cell
+    else:
+        break
+    b -= constant
+    constant += 1
+
+d = depth - 1
+e = width - 2
+f = length - 2
+the_maze[d][e + 1][f] = cell
+constant = 1
+while True:
+    if constant == 1:
+        if the_maze[d][e][f] == wall:
+            the_maze[d][e][f] = cell
+        else:
+            break
+    e -= constant
+    if the_maze[d][e][f] == wall:
+        the_maze[d][e][f] = cell
+    else:
+        break
+    e += constant
+    f -= constant
+    if the_maze[d][e][f] == wall:
+        the_maze[d][e][f] = cell
+    else:
+        break
+    f += constant
+    constant += 1
+
+print_maze(the_maze, length, width, depth)  # prints the maze to begin the fun!
--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # MazeGeneratorRepo
- 
+
+IMPORTANT: It's a good idea to read this readme because it explains how the code works and how to solve the mazes yourself.
+
+BONUS IMPORTANT: It's useful to use a type of .txt font that has "monospacing," AKA all the letters in the text are the same size. That way, mazes don't look horrifying. If you use notepad to open up these .txt files, go to "format" at the top of the screen, click on "font...", and type in "fixedsys". That should give you a font that evenly spaces everything out. If not, look up monospace fonts online. I am sure some sites will give you info about it.
+
+DISCLAIMER: It is recommended you figure out how 3d mazes work first before trying out how 4d mazes work.
+
+With those three out of the way, welcome to my repository of maze generation.
+
+The simple goal of these mazes is to go from the entrance at the top left to the exit at the bottom right by traveling through the mess of directions the maze makes you go through.
+
+There
+
+
+are three files (2d, 3d, and 4d maze generator) that are designed to create mazes based on the desired dimension size. 
+
+To use these files, download or copy the .py files in the "MazeGenerators" folder and run them on whatever app you use to run python files.
+
+To explain how they work, it's a good idea to get a grasp of higher dimensions and what even a 3d maze is.
+
+A 2d maze is a maze a lot of people will be used to. It's a flat maze on a piece of paper that you draw a line from the entrance to the exit. 
+
+A 3d maze isn't a real-life corn maze or a 2d maze that is made in 3d, but rather instead a large cube that has some tunnel in it that leads from the entrance to the exit. The maze cannot only go N, S, E, and W but can also go up and down.
+
+My code makes a 3d maze by giving you a collection of 2d slices of the 3d maze. For example, a 10x10x10 maze would give you 10 10x10 "maps" that would correspond to how far deep you are in the maze. 4d mazes are a bit more unique, however.
+
+My code makes a 4d maze by giving you a collection of 3d mazes that have their own 2d slices. For example, a 10x10x10x10 maze would give you 10 10x10x10 mazes you could adventure in.
+
+For a 4d maze to be possible, you need two new directions that our 3d world doesn't have, which I call "forward" and "backward".
+
+"Forward" sends you to the next 3d maze (object), whereas "backward" sends you to the previous 3d object.
+
+So with a 2d maze, you have to get from the top left of the maze to the bottom right.
+
+With a 3d maze, you have to get from the top left of the highest part of the maze to the bottom right of the bottom part of the maze. This means you have to go up and down through the maze to reach the bottom level and find the exit.
+
+A 4d maze requires you to go from the top left on the highest point of the first object to the bottom right on the lowest level of the last object. This means you have to go forwards and backward through the different 3d objects to get to the last object's bottom level and find the exit.
+
+Did that make at least some sense? Good, because it's time we talk about some technical things with the maze generators.
+
+2d mazes should be straightforward. cells (or 'C') mean you can walk through them. Walls (or '#') block your path, and you can't go through them. This applies to all mazes as well.
+
+Because of how the 3d maze generator works, ups and downs don't necessarily mean they only go up or down. For example, if you find a tunnel that leads down, with the next cell below saying it's a tunnel that leads up, there could technically be a third cell below that, which also says it goes up. This means that ups and downs don't always mean you are forced to go back, so you should always check the level beneath or above in case there's another path. It's probably a good time to talk about how to use 2d slices.
+
+In a 3d maze, when you go "down," you remember what location you were at in the last slice and go to that same coordinate in the 2d slice below. I sadly can't give you the best example of this in a readme file because of how messed up it looks, but if you look at the 3dmazes.txt file and look at "TEACUP PRISON", you should get a good idea of what going down through 2d slices means.
+
+Also, just like how in a 2d maze if you are going forward and see a path to the right and don't have to go right, it's the same case in 3d, in which if you see an up or a down in your path, it doesn't prevent you from walking past it to go to another up or down. Remember, only walls block you.
+
+4d mazes get a bit more hectic because of how many 2d slices are required to... 'render' them. When going forwards and backward in a 4d maze, remember what layer in the current maze you are in, where you are at in that layer, and if you have to move an object backward or forwards in the maze.
+
+Also, be aware that just in the way that ups and downs can overwrite each other in a 3d maze, this is also the case in a 4d maze with forward and backward. Not only should you check layers of a 4d maze for overwritten ups and downs, but you should also check if an up or a down is hiding a secret forwards or backward that could help get you to the exit of a maze.
+
+I can also prove that the 4d mazes are 4d through my idea called "The Tunnel Problem."
+
+. Think about the idea of a 4d maze trying to be made in our world. To go "forward" and "backward" into different objects, you would have to build a tunnel that goes from one object to the next. This tunnel in a 3d world would obstruct a part of the maze, making it not a 4d maze. However, if you were to do the same forward and backward idea in a 4d world, you could effortlessly connect different objects without the tunnels obstructing any other part of the maze. Thus, this proves my 4d maze is 4d because the forward and backward tunnels do not block large parts of the maze from being used.
+
+With the ideas of how 3d and 4d mazes work finished, Let's now talk about a bit of the code.
+
+. I used a maze generation algorithm called "randomized Prim's algorithm," in which you start with a maze that is completely made up of walls, randomly choose one of those walls to be a cell, and then branch out the rest of the maze from that one point.
+
+. (As explained in the py files,) You can edit the dimension's lengths in any way to customize how you want it. You can also edit how the maze looks or what colors the different tunnels represent.
+
+. It's a good idea to read many of the comments in the py files to get a grasp on how the code works. If your computer can't use colorama, or you don't wish to install colorama, follow the commented steps at the top of each py file to remove the parts of code that use it.
+
+Other than these points of how the generators work, have fun generating the mazes and trying to solve them yourself. I had hours of fun playing through these nightmares of tunnels and tricks, and I hope you do too. Be sure to report if you found any bugs or problems I should know about to help improve the code.
+
+Have a great day!
